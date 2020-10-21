f7bb3611a01357536e68122884f7103f042fd673
YaacovSimmons <71831875+YaacovSimmons@users.noreply.github.com>
2020-10-21 12:49:40 +0100

    Contact us page baseline

---
+++ Temp Site/contactus.php
@@ -0,0 +1,21 @@
+<?php
+    include("navigationbar.php"); /* This will include the navigation bar to the page */
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+      <h1>Contact Us</h1>
+      <h3>Please use the form below to contact us:</h3>
+      
+
+      <form>
+            Username:<input type="text" name="Username" required><br><br>
+            
+            Message:<input type="text" name="Message" required><br><br>
+
+            <input type="submit" name="Submit" value="Submit">
+
+      </form>
