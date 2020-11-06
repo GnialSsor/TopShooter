f9a404c13464faf30cb10f8462cb41c758fa605a
AidenDeegan00 <71882873+AidenDeegan00@users.noreply.github.com>
2020-11-06 14:22:22 +0000

    rough estimate of death check code

---
+++ Temp Site/deadcheck.js
@@ -0,0 +1,14 @@
+private bool check() {
+    bool dead = false;
+    if (dead == true) {
+    dead = true;
+    return deathscreen
+    }
+    else if (playertotal == 1) {
+    return deathscreen
+    }
+    else{ 
+    dead = false
+    return dead
+    }
+}
