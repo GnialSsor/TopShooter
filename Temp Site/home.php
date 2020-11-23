3d58ceb9e762af41b5c25f9b15360d3cd269da02
YaacovSimmons <71831875+YaacovSimmons@users.noreply.github.com>
2020-11-23 12:37:57 +0000

    Update home.php
    include database connection
    
    Co-Authored-By: if2001 <71988488+if2001@users.noreply.github.com>

--- Temp Site/home.php
+++ Temp Site/home.php
@@ -1,16 +1,9 @@
 <?php
-    include("navigationbar.php"); /* This will include the navigation bar to the page */
+    include("navigationbar.php"); /* This will include the navigation bar which contains css to the page */
+    include("databaseconnection.php"); /* This will include the database connection */
 
-$connection =mysqli_connect("localhost", "HNDCSSA19", "rAhXexAcqr", "HNDCSSA19")
-or die ('Could not connect to mysql');
-
-$query = "SELECT * FROM >>>Put database connection here<<<"; /* put in db connection (Table name) */
-$result = mysqli_query($connection,$query)
-or die ('could not execute query');							
-while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){ echo
-    
-  
-	'<div class="container">
+/*Creates button to access game */
+'<div class="container">
 <a href=gameMap.php class="btn btn-primary">Play</a>
 </div>'
     
