9804548dd8ae1a421a9e449ae97ac082780fbd6c
YaacovSimmons <71831875+YaacovSimmons@users.noreply.github.com>
2020-11-23 12:28:02 +0000

    Update home.php
    Made changes to home page, added 'Play' button
    
    Co-Authored-By: if2001 <71988488+if2001@users.noreply.github.com>

--- Temp Site/home.php
+++ Temp Site/home.php
@@ -11,23 +11,7 @@ while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){ echo
     
   
 	'<div class="container">
-		<div class="row">
-        <div class="column">
- 			<div class="col-18">
-				<div class="card" border-success mb-3" style="max-width: 20rem;">
-
-					<h5 class="card-title">'.$row['Title'].'</h5>
-					<p class="card-text">'.$row['Description'].'</p>
-                    <img class="" src="'.$row['Image'].'"  alt="" height="300" width="200" >
-                    <div class="col">
-					<div class="card-body">
-                    <a href=game.php?id='.$row[ID].' class="btn btn-primary">Play</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+<a href=gameMap.php class="btn btn-primary">Play</a>
 </div>'
     
 ;} ?>
